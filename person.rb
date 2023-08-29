188bc6ec39e3780a24eb2b21cf783538cb6f4204
yemidada <yemidada@gmail.com>
2023-08-29 16:31:11 +0100

    Add decorator, trimmerDecorator, capitalizeDecorator and nameable files

--- person.rb
+++ person.rb
@@ -1,4 +1,8 @@
-class Person
+require './nameable.rb'
+require './capitalizeDecorator.rb'
+require './trimmerDecorator.rb'
+
+class Person < Nameable
   attr_accessor :id, :name, :age, :parent_permission
 
   def initialize(id, name = 'Unknown', age = 0, parent_permission: true)
@@ -6,6 +10,10 @@ class Person
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private
@@ -20,3 +28,12 @@ class Person
     of_age? || @parent_permission
   end
 end
+
+
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
