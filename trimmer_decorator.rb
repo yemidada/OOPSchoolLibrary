8025f3c912c287ace20456d04f26ad81ff9fbb9e
yemidada <yemidada@gmail.com>
2023-08-29 16:56:12 +0100

    Update trimmer decorator

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,11 +2,11 @@ require './decorator'
 
 class TrimmerDecorator < Decorator
   def correct_name
-    name_variable = @nameable.correct_name
-    if namname_variablee.length > 10
-      name_variable[0, 10]
+    name = @nameable.correct_name
+    if name.length > 10
+      name[0, 10]
     else
-      name_variable
+      name
     end
   end
 end
