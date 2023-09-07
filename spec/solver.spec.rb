d8512fe25417a20ec56ff7bc684ca0dbee8bd5db
BenM4k <bennymakomo@gmail.com>
2023-09-07 13:07:27 +0200

    test for factorial

--- spec/solver.spec.rb
+++ spec/solver.spec.rb
@@ -3,22 +3,30 @@ require_relative '../solver'
 describe Solver do
     solver = Solver.new
     context "when calculating factorial" do
+        it "Should return 1" do
+            expect(solver.factorial(0)).to eq(1)
+        end
+
+        it "should return the real answer" do 
+            num = 5
+            expect(solver.factorial(num)).to eq(120)
+        end
     end
 
     contet "When checking FizzBuzz" do
-        it "should return Fizz" do
+        it "should return fizz" do
             multiple_of_3 = 6
-            expect(solver.fizzbuzz(multiple_of_3)).to eq('Fizz')
+            expect(solver.fizzbuzz(multiple_of_3)).to eq('fizz')
         end
 
-        it "Should return Buzz" do
+        it "Should return fuzz" do
             multiple_of_5 = 10
-            expect(solver.fizzbuzz(multiple_of_5)).to eq('Buzz')
+            expect(solver.fizzbuzz(multiple_of_5)).to eq('buzz')
         end
 
-        it "Should return FizzBuzz" do
+        it "Should return fizzbuzz" do
             common_multiple = 15
-            expect(solver.fizzbuzz(common_multiple)).to eq('FizzBuzz')
+            expect(solver.fizzbuzz(common_multiple)).to eq('fizzbuzz')
         end
 
         it "Should return the number" do
