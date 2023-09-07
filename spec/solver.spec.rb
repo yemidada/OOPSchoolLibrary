089fb71c2448281ede7b47e38fae243b244e274c
BenM4k <bennymakomo@gmail.com>
2023-09-07 13:03:32 +0200

    test for fizzbuzz

--- spec/solver.spec.rb
+++ spec/solver.spec.rb
@@ -6,6 +6,25 @@ describe Solver do
     end
 
     contet "When checking FizzBuzz" do
+        it "should return Fizz" do
+            multiple_of_3 = 6
+            expect(solver.fizzbuzz(multiple_of_3)).to eq('Fizz')
+        end
+
+        it "Should return Buzz" do
+            multiple_of_5 = 10
+            expect(solver.fizzbuzz(multiple_of_5)).to eq('Buzz')
+        end
+
+        it "Should return FizzBuzz" do
+            common_multiple = 15
+            expect(solver.fizzbuzz(common_multiple)).to eq('FizzBuzz')
+        end
+
+        it "Should return the number" do
+            num = 4
+            expect(solver.fizzbuzz(num)).to eq(4)
+        end
     end
 
     context "When reversing a string" do
